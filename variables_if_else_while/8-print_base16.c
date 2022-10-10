4dccc8682412f41155bd7fd737d4fa7e29a4f249
esteban <juan.esteban.94@outlook.com>
2022-10-10 14:28:21 -0500

    Task 8

---
+++ variables_if_else_while/8-print_base16.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+/**
+* main - Prints the hexadecimal numbers.
+* Return: Return 0.
+*/
+int main(void)
+{
+	char i;
+
+	for (i = '0' ; i <= '9'; i++)
+		putchar(i);
+	for (i = 'a' ; i <= 'f'; i++)
+		putchar(i);
+	putchar('\n');
+	return (0);
+}
