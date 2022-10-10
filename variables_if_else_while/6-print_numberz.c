5a377b18a008a3f10de1120b95a06508d999d249
esteban <juan.esteban.94@outlook.com>
2022-10-10 13:21:03 -0500

    Task 6

---
+++ variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+/**
+ * main - Print a natural numbers.
+ * Return: Return 0.
+*/
+int main(void)
+{
+	int i;
+
+	for (i = '0'; i <= '9'; i++)
+		putchar(i);
+putchar('\n');
+return (0);
+}
