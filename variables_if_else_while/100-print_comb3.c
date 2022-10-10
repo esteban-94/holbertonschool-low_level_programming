be054f0aba75d769e091c39dffe508d460524331
esteban <juan.esteban.94@outlook.com>
2022-10-10 16:09:41 -0500

    Task 100

---
+++ variables_if_else_while/100-print_comb3.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+/**
+* main - Prints the numbers with comma without repeat.
+* Return: Return 0.
+*/
+int main(void)
+{
+	int i, k;
+
+	for (i = '0' ; i <= '9'; i++)
+	{
+		for (k = i + 1; k <= '9'; k++)
+		{
+				putchar(i);
+				putchar(k);
+				if (i < '8' && k <='9')
+				{
+					putchar(',');
+					putchar(' ');
+				}
+		}
+	}
+	putchar('\n');
+	return (0);
+}
