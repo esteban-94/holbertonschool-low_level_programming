e753011c764f54e597c2295318c8ff179569542e
esteban <juan.esteban.94@outlook.com>
2022-10-10 13:13:54 -0500

    Task 5

---
+++ variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+/**
+ * main - Print a natural numbers.
+ * Return: Return 0.
+*/
+int main(void)
+{
+	int i;
+
+	for (i = 0; i <= 9; i++)
+		printf("%i", i);
+printf("\n");
+return (0);
+}
