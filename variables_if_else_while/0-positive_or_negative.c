f3d16de708c8a3c119348ae6d47d90c95de2a7a8
esteban <juan.esteban.94@outlook.com>
2022-10-10 11:47:20 -0500

    Task 0

--- variables_if_else_while/0-positive_or_negative.c
+++ variables_if_else_while/0-positive_or_negative.c
@@ -1,7 +1,8 @@
 #include <stdlib.h>
+#include <stdio.h>
 #include <time.h>
-/** 
- * main - Print a rondom number.
+/**
+ * main - Print a random number and his description.
  * Return: Return 0.
 */
 int main(void)
@@ -10,6 +11,11 @@ int main(void)
 
 	srand(time(0));
 	n = rand() - RAND_MAX / 2;
-	/* your code goes there */
-	return (0);
+	if (n > 0)
+		printf("%i is positive\n", n);
+	else if (n == 0)
+		printf("%i is zero\n", n);
+	else
+		printf("%i is negative\n", n);
+return (0);
 }
