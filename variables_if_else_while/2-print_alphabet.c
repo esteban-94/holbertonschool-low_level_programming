616fd961269a8b26b570683cc63b4de1423268d1
esteban <juan.esteban.94@outlook.com>
2022-10-10 12:38:00 -0500

    Task 2

---
+++ variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+/**
+* main - Prints the alphabet in lowercase.
+* Return: Return 0.
+*/
+int main(void)
+{
+	char je;
+
+	for (je = 'a' ; je <= 'z'; je++)
+		putchar(je);
+	putchar('\n');
+	return (0);
+}
