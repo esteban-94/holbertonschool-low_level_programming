b96aff589555651da638c3bcebf4455548082093
esteban <juan.esteban.94@outlook.com>
2022-10-11 15:11:22 -0500

    rask 6

---
+++ functions_nested_loops/6-abs.c
@@ -0,0 +1,12 @@
+#include "main.h"
+/**
+ * _abs - prints absolute value of an integer.
+ * Return: Return 1 if is alphabetic character or 0 otherwise.
+ */
+int _abs(int n)
+{
+	if (n < 0)
+		return (n * -1);
+	else
+		return (n);
+}
