04081bc7e8234bba7897f7e830a5b2f5300d4716
esteban <juan.esteban.94@outlook.com>
2022-10-11 14:41:53 -0500

    task 5

---
+++ functions_nested_loops/5-sign.c
@@ -0,0 +1,23 @@
+#include "main.h"
+/**
+ * _print_sign - checks for alphabetic character.
+ *@n: to prove
+ * Return: Return 1 if is alphabetic character or 0 otherwise.
+ */
+int print_sign(int n)
+{
+	if (n > 0)
+    {
+        _putchar(43);
+		return (1);
+    }
+	else if (n == 0)
+	{
+        _putchar(48);
+        return (0);
+    }
+	{
+        _putchar(45);
+        return (-1);
+    }
+}
