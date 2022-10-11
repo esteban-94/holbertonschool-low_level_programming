fc6e4b03aca5ea46779bc8c5a378062f8e83e1a1
esteban <juan.esteban.94@outlook.com>
2022-10-11 15:19:17 -0500

    task 7

---
+++ functions_nested_loops/7-print_last_digit.c
@@ -0,0 +1,9 @@
+#include "main.h"
+/**
+ * print_last_digit - prints last digit of an integer.
+ * Return: Return 1 if is alphabetic character or 0 otherwise.
+ */
+int print_last_digit(int n)
+{
+	return (n % 10);
+}
