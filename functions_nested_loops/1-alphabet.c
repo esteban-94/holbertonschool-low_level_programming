554a53b0c8150dfd205995f3298d41d466508f9f
esteban <juan.esteban.94@outlook.com>
2022-10-11 13:05:58 -0500

    task 1

--- functions_nested_loops/1-alphabet.c
+++ functions_nested_loops/1-alphabet.c
@@ -3,13 +3,13 @@
  * main - Print alphabet.
  * Return: Return 0.
  */
-int main(void)
+void print_alphabet(void)
 {
 	char i;
 
 	for (i = 'a' ; i <= 'z'; i++)
 	{
-		_putchar('i');
+		_putchar(i);
 	}
 	_putchar('\n');
 	return (0);
