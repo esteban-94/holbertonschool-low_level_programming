c3d968d2cc14cb989f3fab4ece776ac2be2e97a5
esteban <juan.esteban.94@outlook.com>
2022-10-11 22:08:50 -0500

    task 101

--- functions_nested_loops/101-natural.c
+++ functions_nested_loops/101-natural.c
@@ -8,9 +8,7 @@ int main(void)
     int i, j;
     for (i = 1; i < 1024; i++)
     {
-        if ((i % 5) == 0)
-            j = i + j;
-        if ((i % 3) == 0)
+        if ((i % 5) == 0 || (i % 3) == 0)
             j = i + j;
     }
     printf("%i", j);
