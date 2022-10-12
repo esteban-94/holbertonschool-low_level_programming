5523a6376b1c89f4fc86f90d856f31cb2c4b8c32
esteban <juan.esteban.94@outlook.com>
2022-10-12 12:17:05 -0500

    task 7

--- functions_nested_loops/7-print_last_digit.c
+++ functions_nested_loops/7-print_last_digit.c
@@ -1,18 +1,19 @@
 #include "main.h"
 /**
  * print_last_digit - prints last digit of an integer.
- * Return: Return 1 if is alphabetic character or 0 otherwise.
+ *@n: to prove
+ * Return: Return last digit of an integer.
  */
 int print_last_digit(int n)
 {
 	char i, j;
-    i = n % 10;
-    j = -1 * i;
-    if (n < 0)
-    {
-        _putchar(j + '0');
-	    return (j);
-    }
-    _putchar(i + '0');
-    return (i);
+	i = n % 10;
+	j = -1 * i;
+	if (n < 0)
+	{
+		_putchar(j + '0');
+		return (j);
+	}
+	_putchar(i + '0');
+	return (i);
 }
