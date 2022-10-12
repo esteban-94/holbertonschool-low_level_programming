10af8f92b4478c08f122164201bde87b8bab2fc2
esteban <juan.esteban.94@outlook.com>
2022-10-11 19:17:36 -0500

    Task 11

---
+++ functions_nested_loops/11-print_to_98.c
@@ -0,0 +1,15 @@
+#include "main.h"
+#include <stdio.h>
+/**
+ * print_to_98 - prints until 98.
+ * Return: Return 1 if is alphabetic character or 0 otherwise.
+ */
+void print_to_98(int n)
+{
+    int i;
+
+    if (n <= 98)
+        for (i = n; i=98; i++)
+            printf("%i, ", i);
+    
+}
