6a45f703a87836daf5fa8b7296a3f01217d7fdd0
esteban <juan.esteban.94@outlook.com>
2022-10-11 23:31:23 -0500

    task

---
+++ functions_nested_loops/103-fibonacci.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+/**
+ * print_to_98 - prints until 98.
+ * Return: Return 1 if is alphabetic character or 0 otherwise.
+ */
+int main(void)
+{
+    long int i, x, y, z;
+    x = 1;
+    y = 0;
+    for (i = 0; i <= 49; i++)
+    {
+        z = x + y;
+        y = x;
+        x = z;
+        printf("%ld", z);
+        if (i < 49)
+            printf(", ");
+    }
+    printf("\n");
+    return (0);
+}
