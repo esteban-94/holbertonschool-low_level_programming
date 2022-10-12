93a7ae712e1aed10a5a2e0a93cdf0454621952db
esteban <juan.esteban.94@outlook.com>
2022-10-11 20:30:11 -0500

    task 100

---
+++ functions_nested_loops/100-times_table.c
@@ -0,0 +1,31 @@
+#include "main.h"
+/**
+ * print_times_table - prints n times table.
+ * Return: Return 1 if is alphabetic character or 0 otherwise.
+ */
+void print_times_table(int n)
+{
+	int i, j, k;
+
+	for (i = 0; i <= n; i++)
+	{
+		for (j = 0; j <= n; j++)
+		{
+            k = i * j;
+            if ((k / 10) == 0 && j != 0)
+                _putchar(' ');
+            else if (j != 0)
+            {
+                _putchar(k / 100 +'0');
+			    _putchar(k / 10 +'0');
+            }
+			_putchar(k % 10 +'0');
+            if (j < n)
+            {
+                _putchar(',');
+                _putchar(' ');
+            }
+        }
+        _putchar('\n');
+    }
+}
