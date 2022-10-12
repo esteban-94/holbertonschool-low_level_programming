44a0b2835f47a0929776da8ec433485188f29d56
esteban <juan.esteban.94@outlook.com>
2022-10-12 10:41:48 -0500

    task 5

--- functions_nested_loops/5-sign.c
+++ functions_nested_loops/5-sign.c
@@ -1,23 +1,23 @@
 #include "main.h"
 /**
- * _print_sign - checks for alphabetic character.
+ * _print_sign - prints the sign of a integrer.
  *@n: to prove
- * Return: Return 1 if is alphabetic character or 0 otherwise.
+ * Return: Return 1 if is positive number, 0 if is 0, -1 in otherwise.
  */
 int print_sign(int n)
 {
 	if (n > 0)
-    {
-        _putchar(43);
+	{
+		_putchar(43);
 		return (1);
-    }
+	}
 	else if (n == 0)
 	{
-        _putchar(48);
-        return (0);
-    }
+		_putchar(48);
+		return (0);
+	}
 	{
-        _putchar(45);
-        return (-1);
-    }
+		_putchar(45);
+		return (-1);
+	}
 }
