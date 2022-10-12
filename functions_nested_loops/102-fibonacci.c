bcf724dac3726cd6aea89d77a4bf5a83bfb29a75
esteban <juan.esteban.94@outlook.com>
2022-10-12 15:10:05 -0500

    task 102

--- functions_nested_loops/102-fibonacci.c
+++ functions_nested_loops/102-fibonacci.c
@@ -1,22 +1,22 @@
 #include <stdio.h>
 /**
- * print_to_98 - prints until 98.
- * Return: Return 1 if is alphabetic character or 0 otherwise.
+ * main - prints fibonacci numbers.
+ * Return: Return 0 value.
  */
 int main(void)
 {
-    long int i, x, y, z;
-    x = 1;
-    y = 0;
-    for (i = 0; i <= 49; i++)
-    {
-        z = x + y;
-        y = x;
-        x = z;
-        printf("%ld", z);
-        if (i < 49)
-            printf(", ");
-    }
-    printf("\n");
-    return (0);
+	long int i, x, y, z;
+	x = 1;
+	y = 0;
+	for (i = 0; i <= 49; i++)
+	{
+		z = x + y;
+		y = x;
+		x = z;
+		printf("%ld", z);
+		if (i < 49)
+			printf(", ");
+	}
+	printf("\n");
+	return (0);
 }
