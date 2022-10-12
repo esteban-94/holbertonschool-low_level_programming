b34b5cfc5e74c5b3f7cd5d1f968c4c27afdf6e18
esteban <juan.esteban.94@outlook.com>
2022-10-12 10:10:09 -0500

    task 3

--- functions_nested_loops/3-islower.c
+++ functions_nested_loops/3-islower.c
@@ -2,16 +2,12 @@
 /**
  * _islower - checks for lowercases.
  *@c: to prove
- * Return: Return 1 if is lowercase or 0 otherwise.
+ * Return: Return 1 if a letter is lowercase or 0 otherwise.
  */
 int _islower(int c)
 {
 	if (c > 96 && c <= 122)
-	{
 		return (1);
-	}
 	else
-	{
 		return (0);
-	}
 }
