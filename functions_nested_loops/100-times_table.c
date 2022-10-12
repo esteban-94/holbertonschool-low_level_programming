f519dacaea5224d67ac168b0bbc1c377cf928007
esteban <juan.esteban.94@outlook.com>
2022-10-11 21:22:36 -0500

    Task 100

--- functions_nested_loops/100-times_table.c
+++ functions_nested_loops/100-times_table.c
@@ -7,40 +7,43 @@ void print_times_table(int n)
 {
 	int i, j, k;
 
-	for (i = 0; i <= n; i++)
-	{
-		for (j = 0; j <= n; j++)
-		{
-            k = i * j;
-            if (j == 0)
-                _putchar('0');
-            else
+    if (0 <= n && n <= 15 )
+    {
+        for (i = 0; i <= n; i++)
+        {
+            for (j = 0; j <= n; j++)
             {
-                if (10 <= k && k < 100)
-                {
-                    _putchar(' ');
-                    _putchar(k / 10 +'0');
-                    _putchar(k % 10 +'0');
-                }
-                else if (k < 10)
-                {
-                    _putchar(' ');
-                    _putchar(' ');
-                    _putchar(k % 10 +'0');
-                }
+                k = i * j;
+                if (j == 0)
+                    _putchar('0');
                 else
                 {
-                    _putchar(k / 100 +'0');
-                    _putchar((k / 10) % 10 +'0');
-                    _putchar(k % 10 +'0');
+                    if (10 <= k && k < 100)
+                    {
+                        _putchar(' ');
+                        _putchar(k / 10 +'0');
+                        _putchar(k % 10 +'0');
+                    }
+                    else if (k < 10)
+                    {
+                        _putchar(' ');
+                        _putchar(' ');
+                        _putchar(k % 10 +'0');
+                    }
+                    else
+                    {
+                        _putchar(k / 100 +'0');
+                        _putchar((k / 10) % 10 +'0');
+                        _putchar(k % 10 +'0');
+                    }
+                }
+                if (j < n)
+                {
+                    _putchar(',');
+                    _putchar(' ');
                 }
             }
-            if (j < n)
-            {
-                _putchar(',');
-                _putchar(' ');
-            }
+            _putchar('\n');
         }
-        _putchar('\n');
     }
 }
