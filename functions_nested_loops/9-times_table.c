7cad130d14e7dab1aeb14544b8d90fc9e567e025
esteban <juan.esteban.94@outlook.com>
2022-10-12 12:26:50 -0500

    task 9

--- functions_nested_loops/9-times_table.c
+++ functions_nested_loops/9-times_table.c
@@ -15,8 +15,8 @@ void times_table(void)
 			if ((k / 10) == 0 && j != 0)
 				_putchar(' ');
 			else if (j != 0)
-				_putchar(k / 10 +'0');
-			_putchar(k % 10 +'0');
+				_putchar((k / 10) + '0');
+			_putchar((k % 10) + '0');
 			if (j < 9)
 			{
 				_putchar(',');
