7472d5ab9d3ec006a1535237358f86cc9daf3fd9
esteban <juan.esteban.94@outlook.com>
2022-10-12 14:25:50 -0500

    task 100

--- functions_nested_loops/100-times_table.c
+++ functions_nested_loops/100-times_table.c
@@ -21,21 +21,21 @@ void print_times_table(int n)
 				{
 					if (10 <= k && k < 100)
 					{
-					    _putchar(' ');
-					    _putchar(k / 10 +'0');
-					    _putchar(k % 10 +'0');
+						_putchar(' ');
+						_putchar((k / 10) + '0');
+						_putchar((k % 10) + '0');
 					}
 					else if (k < 10)
 					{
-					    _putchar(' ');
-					    _putchar(' ');
-					    _putchar(k % 10 +'0');
+						_putchar(' ');
+						_putchar(' ');
+						_putchar((k % 10) + '0');
 					}
 					else
 					{
-						_putchar(k / 100 +'0');
-						_putchar((k / 10) % 10 +'0');
-						_putchar(k % 10 +'0');
+						_putchar((k / 100) + '0');
+						_putchar((k / 10) % 10 + '0');
+						_putchar((k % 10) + '0');
 					}
 				}
 				if (j < n)
