69d1e905d53daa064ca92eace5465c61857c9a83
esteban <juan.esteban.94@outlook.com>
2022-10-11 19:36:48 -0500

    task 11

--- functions_nested_loops/11-print_to_98.c
+++ functions_nested_loops/11-print_to_98.c
@@ -12,9 +12,9 @@ void print_to_98(int n)
     {
         for (i = n; i <= 98; i++)
         {
-            if (i == 98)
-                printf("%i", i);
-            printf("%i, ", i);
+            printf("%i", i);
+            if (i != 98)
+                printf(", ");
         }
     }
     else
