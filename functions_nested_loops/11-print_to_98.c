94688d870a18217b60b6fd9fa86d4bab51d96e43
esteban <juan.esteban.94@outlook.com>
2022-10-12 12:36:17 -0500

    task 11

--- functions_nested_loops/11-print_to_98.c
+++ functions_nested_loops/11-print_to_98.c
@@ -1,33 +1,34 @@
 #include "main.h"
 #include <stdio.h>
 /**
- * print_to_98 - prints until 98.
- * Return: Return 1 if is alphabetic character or 0 otherwise.
+ * print_to_98 - prints numbers up to 98.
+ * @n: to prove
+ * Return: void function not have return.
  */
 void print_to_98(int n)
 {
-    int i;
+	int i;
 
-    if (n <= 98)
-    {
-        for (i = n; i <= 98; i++)
-        {
-            printf("%i", i);
-            if (i != 98)
-                printf(", ");
-            else if (i == 98)
-                printf("\n");
-        }
-    }
-    else
-    {
-        for (i = n; i >= 98; i--)
-        {
-            printf("%i", i);
-            if (i != 98)
-                printf(", ");
-            else if (i == 98)
-                printf("\n");
-        }
-    }   
+	if (n <= 98)
+	{
+		for (i = n; i <= 98; i++)
+		{
+			printf("%i", i);
+			if (i != 98)
+				printf(", ");
+			else if (i == 98)
+				printf("\n");
+		}
+	}
+	else
+	{
+		for (i = n; i >= 98; i--)
+		{
+			printf("%i", i);
+			if (i != 98)
+				printf(", ");
+			else if (i == 98)
+				printf("\n");
+		}
+	}
 }
