5e8cbede4d4fe787575ee63ff1b3f50d5d884f3b
esteban <juan.esteban.94@outlook.com>
2022-10-13 10:26:49 -0500

    task 6

---
+++ more_functions_nested_loops/6-print_line.c
@@ -0,0 +1,15 @@
+#include "main.h"
+/**
+ * print_line - Print a line.
+ * Return: void function not have return.
+ */
+void print_line(int n)
+{
+	int i;
+
+	for (i = 0 ; i < n; i++)
+	{
+		_putchar(95);
+	}
+	_putchar('\n');
+}
