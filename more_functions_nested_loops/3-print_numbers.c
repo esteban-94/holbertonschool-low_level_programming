1ed23fb58ef136448537cee210465b2beb0b8ea8
esteban <juan.esteban.94@outlook.com>
2022-10-12 22:36:41 -0500

    task 3

---
+++ more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,15 @@
+#include "main.h"
+/**
+ * print_numbers - Print the natural numbers.
+ * Return: void function not have return.
+ */
+void print_numbers(void)
+{
+	int i;
+
+	for (i = 48 ; i <= 57; i++)
+	{
+		_putchar(i);
+	}
+	_putchar('\n');
+}
