5b491a79b449309c35becb1bc4d620ccb66f9126
esteban <juan.esteban.94@outlook.com>
2022-10-12 22:53:40 -0500

    task 5

---
+++ more_functions_nested_loops/5-more_numbers.c
@@ -0,0 +1,16 @@
+#include "main.h"
+/**
+ * more_numbers - Print the numbers from 0 to 14 ten times.
+ * Return: void function not have return.
+ */
+void more_numbers(void)
+{
+	int i, j;
+
+	j = i % 10;
+	for (i = '0' ; i <= '9'; i++)
+	{
+		_putchar(i);
+	}
+	_putchar('\n');
+}
