45738c9ac271802f8da44de00a0a60aecffd0721
esteban <juan.esteban.94@outlook.com>
2022-10-13 10:40:21 -0500

    task 7

--- more_functions_nested_loops/7-print_diagonal.c
+++ more_functions_nested_loops/7-print_diagonal.c
@@ -12,9 +12,9 @@ void print_diagonal(int n)
 	{
         for (j = 0 ; j < n; j++)
         {
-		    _putchar(0);
-            _putchar(92);
+		    _putchar(32);
         }
+        _putchar(92);
 	}
 	_putchar('\n');
 }
