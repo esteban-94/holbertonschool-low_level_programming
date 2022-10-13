4ef75f1f1ee92105a161750cd27299dfa5de53d3
esteban <juan.esteban.94@outlook.com>
2022-10-12 22:17:28 -0500

    task 0

--- more_functions_nested_loops/0-isupper.c
+++ more_functions_nested_loops/0-isupper.c
@@ -1,12 +1,12 @@
 #include "main.h"
 /**
- * _islower - checks for lowercases.
+ * _isupper - checks for uppercases.
  *@c: to prove
- * Return: Return 1 if a letter is lowercase or 0 otherwise.
+ * Return: return 1 is c is uppercase.
  */
 int _isupper(int c)
 {
-	if (c > 96 && c <= 122)
+	if (c >= 65 && c <= 90)
 		return (1);
 	else
 		return (0);
