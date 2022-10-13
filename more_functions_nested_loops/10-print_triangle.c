ff5be61ffbf474c543d113d35296801fafc049c5
esteban <juan.esteban.94@outlook.com>
2022-10-13 14:21:14 -0500

    task 10

---
+++ more_functions_nested_loops/10-print_triangle.c
@@ -0,0 +1,25 @@
+#include "main.h"
+#include "main.h"
+/**
+ * print_triangle - Print a triangle with # symbol.
+ *@size: to prove
+ * Return: void function not have return.
+ */
+void print_triangle(int size)
+{
+	int i, j;
+
+	for (i = 0 ; i < size; i++)
+	{
+		for (j = 0 ; j < size; j++)
+		{
+			if (j <= i)
+				_putchar(32);
+			else
+				_putchar(35);
+		}
+		_putchar('\n');
+	}
+	if (size <= 0)
+		_putchar('\n');
+}
