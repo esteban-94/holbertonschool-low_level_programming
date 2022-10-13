700d5e8a31bf69a3efaea1d19235a51de9c25eae
esteban <juan.esteban.94@outlook.com>
2022-10-13 14:54:48 -0500

    task 10

--- more_functions_nested_loops/10-print_triangle.c
+++ more_functions_nested_loops/10-print_triangle.c
@@ -9,9 +9,9 @@ void print_triangle(int size)
 {
 	int i, j;
 
-	for (i = 0 ; i < size; i++)
+	for (i = 1 ; i <= size; i++)
 	{
-		for (j = 0 ; j < size; j++)
+		for (j = 1 ; j <= size; j++)
 		{
 			if (j < (size - i))
 				_putchar(32);
