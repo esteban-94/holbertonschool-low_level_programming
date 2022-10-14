bf7b4caaeea3423ea6b49325911864ccb78d3e36
esteban <juan.esteban.94@outlook.com>
2022-10-13 23:20:15 -0500

    task

--- more_functions_nested_loops/101-print_number.c
+++ more_functions_nested_loops/101-print_number.c
@@ -1,6 +1,7 @@
 #include "main.h"
 /**
- * main - Prints an integer.
+ * print_number - Prints an integer.
+ *@n: to probe
  * Return: Void function not have return.
  */
 void print_number(int n)
@@ -145,6 +146,30 @@ void print_number(int n)
     else if (n >=0 && n <= 9)
     {
         p = n % 10;
+        _putchar(p + 48);
+    }
+    else if (n >=-2147483647 && n <= -1000000000)
+    {
+        q = n / -1000000000;
+        _putchar(45);
+        _putchar(q + 48);
+        w = (n / -100000000) % 10;
+        _putchar(w + 48);
+        e = (n / -10000000) % 10;
+        _putchar(e + 48);
+        r = (n / -1000000) % 10;
+        _putchar(r + 48);
+        t = (n / -100000) % 10;
+        _putchar(t + 48);
+        y = (n / -10000) % 10;
+        _putchar(y + 48);
+        u = (n / -1000) % 10;
+        _putchar(u + 48);
+        i = (n / -100) % 10;
+        _putchar(i + 48);
+        o = (n / -10) % 10;
+        _putchar(o + 48);
+        p = n % -10;
         _putchar(p + 48);
     }
 }
