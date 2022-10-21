40d469ba178c6e02881b03ba15f85c4247de51f8
esteban <juan.esteban.94@outlook.com>
2022-10-21 10:23:54 -0500

    task 0

--- pointers_arrays_strings/0-memset.c
+++ pointers_arrays_strings/0-memset.c
@@ -4,12 +4,13 @@
 * @s: to probe
 * @b: to probe
 * @n: to probe
+* Return: s
 */
 char *_memset(char *s, char b, unsigned int n)
 {
 	unsigned int i;
 
 	for (i = 0; i < n; i++)
-				s[i] = b;
-	return s;
+		s[i] = b;
+	return (s);
 }
