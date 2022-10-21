3a32ca818f1b5dca3b30029c7bd3702470cb7a60
esteban <juan.esteban.94@outlook.com>
2022-10-21 09:32:50 -0500

    task 2

--- pointers_arrays_strings/2-strncpy.c
+++ pointers_arrays_strings/2-strncpy.c
@@ -1,6 +1,6 @@
 #include "main.h"
 /**
-* *_strncat - function task 1
+* *_strncpy - function task 1
 *@dest: to probe
 *@src: to probe
 *@n: to probe
@@ -10,11 +10,10 @@ char *_strncpy(char *dest, char *src, int n)
 {
 	int i;
 
-	for (i = 0 ; i < n; i++)
-	{
-		*(dest + i) = *(src + i);
-		if (*(src + i) == '\0')
-			break;
-	}
+	for (i = 0; i < n && src[i] != '\0'; i++)
+		dest[i] = src[i];
+	for (; i < n; i++)
+		dest[i] = '\0';
+
 	return (dest);
 }
