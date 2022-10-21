eca19c3227cb90263b5a8ed2bc42eee1c158979d
esteban <juan.esteban.94@outlook.com>
2022-10-21 14:20:49 -0500

    task 8

---
+++ pointers_arrays_strings/8-print_diagsums.c
@@ -0,0 +1,19 @@
+#include "main.h"
+#include <stdio.h>
+/**
+ * print_diagsums - function task 8
+ * @a: to probe
+ * @size: to probe
+ */
+void print_diagsums(int *a, int size)
+{
+	int i, j, sum1, sum2;
+
+	j = size * size - 1;
+	for (i = 0; i < size; i++)
+	{
+		sum1 += a[(size * i) + i];
+		sum2 += a[j - (size - 1) - i * (size - 1)];
+	}
+	printf("%d, %d\n", sum1, sum2);
+}
