14dca990bdbff6b5c5ffdc29136beac263a26f0d
esteban <juan.esteban.94@outlook.com>
2022-10-21 11:46:22 -0500

    task 3

---
+++ pointers_arrays_strings/3-strspn.c
@@ -0,0 +1,23 @@
+#include "main.h"
+/**
+* *_strchr - function task 3
+*@s: to probe
+*@accept: to probe
+* Return: i
+*/
+unsigned int _strspn(char *s, char *accept)
+{
+	unsigned int i, j;
+
+	for (i = 0; s[i]; i++)
+	{
+		for (j = 0; accept[j]; j++)
+		{
+			if (s[i] == accept[j])
+				break;
+		}
+		if (s[i] != accept[j])
+			break;
+	}
+	return (i);
+}
