7171e6c89c177389e353ba6e5902da05fb69bb11
esteban <juan.esteban.94@outlook.com>
2022-10-21 10:21:45 -0500

    task 0

--- pointers_arrays_strings/0-memset.c
+++ pointers_arrays_strings/0-memset.c
@@ -1,12 +1,15 @@
 #include "main.h"
 /**
-*
+* *_memset - function task 0
+* @s: to probe
+* @b: to probe
+* @n: to probe
 */
 char *_memset(char *s, char b, unsigned int n)
 {
-    unsigned int i;
+	unsigned int i;
 
-    for (i = 0; i < n; i++)
-                s[i] = b;
-    return s;
+	for (i = 0; i < n; i++)
+				s[i] = b;
+	return s;
 }
