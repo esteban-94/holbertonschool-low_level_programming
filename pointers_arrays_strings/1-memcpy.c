92df8b8a5e61a4130598c4bc39d81bbee4a5f0a2
esteban <juan.esteban.94@outlook.com>
2022-10-21 10:40:45 -0500

    task 1

--- pointers_arrays_strings/1-memcpy.c
+++ pointers_arrays_strings/1-memcpy.c
@@ -1,12 +1,16 @@
 #include "main.h"
 /**
-*
+* *_memcpy - function task 1
+* @dest: to probe
+* @src: to probe
+* @n: to probe
+* Return: dest
 */
 char *_memcpy(char *dest, char *src, unsigned int n)
 {
-    unsigned int i;
+	unsigned int i;
 
-    for (i = 0; i < n; i++)
-                dest[i] = src[i];
-    return dest;
+	for (i = 0; i < n; i++)
+		dest[i] = src[i];
+	return dest;
 }
