bd317e3b29a8cfc31c8a215b7fccafa7a79875a6
esteban <juan.esteban.94@outlook.com>
2022-10-21 17:19:52 -0500

    task 101

--- pointers_arrays_strings/101-keygen.c
+++ pointers_arrays_strings/101-keygen.c
@@ -7,11 +7,13 @@
 int main(void)
 {
 	int i;
+	char characters[];
 
 	srand((unsigned int)(time(NULL)));
 	
 	i = 0;
-	char characters[] = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789/,.-+=~`<>:";
+	characters[] = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789/,.-+=~`<>:";
 	for(i = 0; i < 10; i++)
 		printf("%c", characters[rand() % (sizeof characters - 1)]);
+	return (0);
 }
