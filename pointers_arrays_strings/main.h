46ddb282e63294c367ad91020459e39ff08e018c
esteban <juan.esteban.94@outlook.com>
2022-10-21 22:18:33 -0500

    task 100

--- pointers_arrays_strings/main.h
+++ pointers_arrays_strings/main.h
@@ -27,3 +27,4 @@ char *_strstr(char *haystack, char *needle);
 void print_chessboard(char (*a)[8]);
 void print_diagsums(int *a, int size);
 void print_number(int n);
+void set_string(char **s, char *to);
