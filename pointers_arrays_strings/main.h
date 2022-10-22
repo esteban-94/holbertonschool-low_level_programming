e1eb5c70772264f0b9479b3d7bb940ca727daf0b
esteban <juan.esteban.94@outlook.com>
2022-10-21 21:49:12 -0500

    task 100

--- pointers_arrays_strings/main.h
+++ pointers_arrays_strings/main.h
@@ -17,6 +17,7 @@ void reverse_array(int *a, int n);
 char *string_toupper(char *scr);
 char *cap_string(char *a);
 char *leet(char *a);
+char *rot13(char *a);
 char *_memset(char *s, char b, unsigned int n);
 char *_memcpy(char *dest, char *src, unsigned int n);
 char *_strchr(char *s, char c);
