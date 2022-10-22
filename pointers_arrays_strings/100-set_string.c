284e66cec22f02862e8f498a03d41a03a65ad996
esteban <juan.esteban.94@outlook.com>
2022-10-21 22:22:45 -0500

    task 100

--- pointers_arrays_strings/100-set_string.c
+++ pointers_arrays_strings/100-set_string.c
@@ -1,11 +1,9 @@
 #include "main.h"
-
 /**
- * set_string - sets the value of a pointer to a char.
- * @s: Pointer to pointer to string in which the change is made
- * @to: Pointer to change
+ * set_string - function task 100
+ * @s: to probe
+ * @to: to probe
  */
-
 void set_string(char **s, char *to)
 {
 	*s = to;
