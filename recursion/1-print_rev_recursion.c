451f9812143242ed808e8f3a20550941982052e0
esteban <juan.esteban.94@outlook.com>
2022-10-25 14:45:49 -0500

    task 1

---
+++ recursion/1-print_rev_recursion.c
@@ -0,0 +1,13 @@
+#include "main.h"
+/**
+ * _puts_recursion - function task 1
+ *@s: to probe
+ */
+void _print_rev_recursion(char *s)
+{
+	if (*s != '\0')
+	{
+		_print_rev_recursion(s + 1);
+		_putchar(*(s - 1));
+	}
+}
