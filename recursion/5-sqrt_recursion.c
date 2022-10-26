6f7e5f1d865de671f586d143597ea773623e1328
esteban <juan.esteban.94@outlook.com>
2022-10-26 09:25:23 -0500

    task 5

---
+++ recursion/5-sqrt_recursion.c
@@ -0,0 +1,28 @@
+#include "main.h"
+/**
+ * _sqrt_recursion - function task 5
+ *@n: to probe
+ * Return: sqrt
+ */
+int _sqrt_recursion(int n)
+{
+	int i;
+
+	i = 1;
+	return fun2(n, i));
+}
+/**
+ *fun2 -auxiliar function
+ *@n: to probe
+ *@i: to probe
+ * Return: i
+ */
+int fun2(int n, int i)
+{
+	if (n / i == i)
+		return (i);
+	else if (i < n)
+		return fun2(n, i + 1));
+	else
+		return (-1);
+}
