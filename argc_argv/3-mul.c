6abf025cccc33d199c779c8724fb6a591c89329b
esteban <juan.esteban.94@outlook.com>
2022-10-28 22:42:05 -0500

    task 3

---
+++ argc_argv/3-mul.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+#include <stdlib.h>
+/**
+ * main - 
+ * @argc: 
+ * @argv: 
+ * Return: 0
+*/
+int main(int argc, char *argv[])
+{
+	int i, j;
+
+	j = 1;
+    for (i = 1; i < argc; i++)
+	{
+		int x = strtol(argv[i], NULL, 10);
+		j = j * x;
+	}
+	if (argc < 3)
+		printf("Error\n");
+	else
+		printf("%d\n", j);
+	return (0);
+}
