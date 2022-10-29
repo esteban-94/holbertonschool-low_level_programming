badabddc2bb66cede263b215c0e5e9535ec9df0c
esteban <juan.esteban.94@outlook.com>
2022-10-28 22:21:02 -0500

    task 1

---
+++ argc_argv/1-args.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+/**
+ * main - funtion task 1
+ * @argc: to probe
+ * @argv: to probe
+ * Return: 0
+*/
+int main(int argc, char *argv[] __attribute__ ((unused)))
+{
+	printf("%d\n", argc - 1);
+	return (0);
+}
