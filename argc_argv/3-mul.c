d8ff46d04a181a3ffe62c3602057373134b7dca6
esteban <juan.esteban.94@outlook.com>
2022-10-28 22:47:44 -0500

    task 3

--- argc_argv/3-mul.c
+++ argc_argv/3-mul.c
@@ -11,9 +11,10 @@ int main(int argc, char *argv[])
 	int i, j;
 
 	j = 1;
-    for (i = 1; i < argc; i++)
+	for (i = 1; i < argc; i++)
 	{
 		int x = strtol(argv[i], NULL, 10);
+		
 		j = j * x;
 	}
 	if (argc < 3)
