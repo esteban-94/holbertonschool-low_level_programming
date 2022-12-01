343f14e1bf64a7dc02a584930741cb965831014d
esteban <juan.esteban.94@outlook.com>
2022-12-01 17:23:40 -0500

    task 100

---
+++ singly_linked_lists/100-first.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+/**
+ * main - check code
+ * Return: 0
+*/
+
+int main(void)
+{
+    printf("You're beat! and yet, you must allow");
+    printf(",\nI bore my house upon my back!\n");
+    return (0);
+}
